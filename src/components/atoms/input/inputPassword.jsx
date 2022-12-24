8a743523cd32fcc59c240662de3488ac0d3c5fd0
ichwn <ichwanarif26@gmail.com>
2022-12-25 00:03:04 +0700

    comment unused code

--- src/components/atoms/input/inputPassword.jsx
+++ src/components/atoms/input/inputPassword.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PasswordInput = ({data, onChange}) => {
-  const [showPassword, setShowPassword] = React.useState(false);
+  // const [showPassword, setShowPassword] = React.useState(false);
 
   return (
     <div className="w-full text-black border-[1px] border-[#BBBBBB] rounded-md px-3 py-2 focus:outline-none flex flex-row">
       <input 
         className="flex-1 focus-visible:outline-none"
-        type={showPassword ? "text" : data.type}
+        // type={showPassword ? "text" : data.type}
         name={data.name ? data.name : ""} 
         placeholder={data.placeholder} 
         autoComplete="off"
